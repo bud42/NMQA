30fb8115ef4650b7219511757f2681d2eab9b051
bud42 <bdboyd42@gmail.com>
2023-10-16 09:59:15 -0400

    Update smooth.m

--- src/smooth.m
+++ src/smooth.m
@@ -4,7 +4,7 @@ spm('defaults', 'fmri');
 spm_jobman('initcfg');
 
 % smoothing with 1mm kernel
-matlabbatch{1}.spm.spatial.smooth.data = { '/OUTPUTS/DATA/wmeanNM.nii,1'};
+matlabbatch{1}.spm.spatial.smooth.data = { '/OUTPUTS/DATA/wmeanNM.nii'};
 matlabbatch{1}.spm.spatial.smooth.fwhm = [1 1 1];
 matlabbatch{1}.spm.spatial.smooth.dtype = 0;
 matlabbatch{1}.spm.spatial.smooth.im = 0;
