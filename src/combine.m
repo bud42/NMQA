22340dc7fb05d1dd16c7e7a7f3a94e0a34db3370
bud42 <bdboyd42@gmail.com>
2024-03-18 15:43:27 -0400

    v2 updates

---
+++ src/combine.m
@@ -0,0 +1,18 @@
+clear matlabbatch;
+fig = spm_figure('CreateWin','Graphics');
+spm('defaults', 'fmri');
+spm_jobman('initcfg');
+
+merged_filename = '/OUTPUTS/DATA/merged_swmeanNM.nii'
+
+files = spm_select('ExtFPListRec', '/INPUTS/PREPROC', '.*');
+input_filenames = {cellstr(files)};
+
+matlabbatch{1}.spm.util.cat.vols = input_filenames;
+matlabbatch{1}.spm.util.cat.name = merged_filename;
+matlabbatch{1}.spm.util.cat.dtype = 4;
+matlabbatch{1}.spm.util.cat.RT = NaN;
+
+spm_jobman('run', matlabbatch);
+
+exit;
---
+++ src/run_combine.sh
@@ -0,0 +1,1 @@
+/opt/spm12/run_spm12.sh /opt/mcr/v92 script /opt/src/combine.m
