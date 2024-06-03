0e1a36f080018fa04ee65c951bbcd3108bcb9dd2
bud42 <bdboyd42@gmail.com>
2024-06-03 19:20:18 -0400

    updates

--- src/analysis/main.sh
+++ src/analysis/main.sh
@@ -1,3 +1,5 @@
 /opt/spm12/run_spm12.sh /opt/mcr/v92 script /opt/src/combine.m
 
 /opt/spm12/run_spm12.sh /opt/mcr/v92 script /opt/src/NM_CR_Map.m
+
+cp /opt/src/Segmentation.nii /OUTPUTS/
---
+++ src/analysis/pre.sh
@@ -0,0 +1,8 @@
+# Make the subjects folder
+mkdir -p /OUTPUTS/DATA/SUBJECTS
+
+# Copy subjects to outputs
+cp -r /INPUTS/* /OUTPUTS/DATA/SUBJECTS/
+ 
+# Decompress for loading in SPM
+gunzip /OUTPUTS/DATA/SUBJECTS/*/*.nii.gz
