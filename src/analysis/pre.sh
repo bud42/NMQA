9b3971f94547f6c2e627e2c6ffa9b6aa4fc2cc9c
bud42 <bdboyd42@gmail.com>
2024-07-16 17:49:01 -0400

    all sessions

--- src/analysis/pre.sh
+++ src/analysis/pre.sh
@@ -2,7 +2,7 @@
 mkdir -p /OUTPUTS/DATA/SUBJECTS
 
 # Copy subjects to outputs
-cp -r /INPUTS/* /OUTPUTS/DATA/SUBJECTS/
+cp -r /INPUTS/*/* /OUTPUTS/DATA/SUBJECTS/
  
 # Decompress for loading in SPM
 gunzip /OUTPUTS/DATA/SUBJECTS/*/*.nii.gz
