ab20dc6f8169fd0bfe7e007991deb776e722c53a
bud42 <bdboyd42@gmail.com>
2024-08-04 18:12:28 -0400

    Update pre.sh

--- src/analysis/pre.sh
+++ src/analysis/pre.sh
@@ -1,8 +1,25 @@
+set -x
+
 # Make the subjects folder
 mkdir -p /OUTPUTS/DATA/SUBJECTS
 
+cd /INPUTS
+
 # Copy subjects to outputs
-cp -r /INPUTS/*/* /OUTPUTS/DATA/SUBJECTS/
- 
+for i in *;do
+
+    cd /INPUTS/$i
+
+    for j in *;do
+
+        # Copy from inputs to outputs
+        cp -r /INPUTS/$i/$j/assessors/* /OUTPUTS/DATA/SUBJECTS/${j}
+
+        # Append to subject list
+        echo ${j} >> /OUTPUTS/subjects.txt
+
+    done
+done
+
 # Decompress for loading in SPM
 gunzip /OUTPUTS/DATA/SUBJECTS/*/*.nii.gz
